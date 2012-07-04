db98b0260e8e437e9ce3ac9bc6baba2efc78d3b3
Andreas Vogel <andreas.vogel@gcsc.uni-frankfurt.de>
2012-07-04 13:46:46 +0000

    Renaming folder ip_data -> user_data

--- level_set.h
+++ level_set.h
@@ -12,7 +12,7 @@
 
 #include <vector>
 #include "lib_disc/common/groups_util.h"
-#include "lib_disc/spatial_disc/ip_data/user_data.h"
+#include "lib_disc/spatial_disc/user_data/user_data.h"
 #include "lib_disc/function_spaces/approximation_space.h"
 #include "lib_disc/spatial_disc/disc_util/finite_volume_geometry.h"
 #include "lib_disc/spatial_disc/constraints/constraint_interface.h"
